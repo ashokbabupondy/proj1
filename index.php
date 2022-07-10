143adba6b0ec16afcb5fa897e1fcfebe173f5ab5
ashokbabupondy <ashokbabu.sonai@gmail.com>
2022-07-10 14:40:20 +0530

    one

---
+++ index.php
@@ -0,0 +1,38 @@
+CTYPE html>
+<html>
+		<head>
+					<ials - Secured Login with php5>
+							<l"stylesheet" t href="style.css" />
+							body>
+								
+									<header id="head" >
+												 <p>Codecall tutorials User Login</p>
+												 		 <p><a hrister.php"><span id="register">Register</span></a></p>
+																   		</header>
+																				
+																						<"main-wrapper">
+																							       		 <div id="rapper">
+																									      			 <tion="">
+																																	 <ul>
+																																	 li>
+																																	 						 <label for="usn">Username : </label>
+																																								     						 ext" maxlength="30" required autofocus name="username" />
+																																														     					 </li>
+																																																			 					
+																																																			 					 <li>
+																																																									 						 <label for="passwd">Password : </						 <input type="password" maxlength="30" required name="password" />
+																																																																 					 </li>
+																																																																					 	i class="buttons">
+																																																																												 <input type="submit" name="login" value="Log me in" />
+																																																																												 							<input type="button" name="register" value="Register" onclick="location.href='register.php'" />
+																																																																																								 </li>
+																																																																																								 					
+																																																																																								 				 </ul>
+																																																																																												 			 </form>
+																																																																																															 				
+																																																																																															 			</div>
+																																																																																																				</div>
+																																																																																																					
+																																																																																																					</body>
+</html>
+
